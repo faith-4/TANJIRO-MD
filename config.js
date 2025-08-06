4ddcf02211edef81e6748b1d851ecfa7abf29150
Nerdk <univversehackers@gmail.com>
2025-08-06 07:39:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,80 +4,53 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ARSLAN-MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiV1BkbXRnandwamdHK1lzd0lkVitNdzIzbDNQcm5JV1JOSEJ6MlI2Y3lFZz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiU2pHVTRORWlqTjVWMG1NNmZiVDBhcGdsK2FvL2Q3UVRjdW5veituZjVpMD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJFT0pqQU14bDNtcEhyMG5ZNWRHNnBWVE5IcWIwaGJRK0ZBL1RFMzUvczNBPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJWMU5hSGZzVnpOTzJLeWtlbS9PWFN5Z2daTEx1aG8zR2xGc2prR2V6dlc4PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InlNRjJZSE1LQ1d6WnlwZmQ1NUVVelJqY1UzTXZlR0pxY1pWMnNybGJ5Rk09In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkZNaGxDTVNKNHNyQ2NoYmxmNUN4ZEVvYjBGRTh3aEV2cTU2OTljU2ZJVTg9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNkRyN2pFWFJhL0ZyUm4vWGhaNjhSajlYa044MHQ4Y3VETW9ZREM0UkdWRT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOUp2MXVaQ1lRNmUyeTlQS2dHditNSmlyd0VhZ3FjKzB3RSsxU3V6WEVWYz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InhueVlPbjQ4TTNCdWthc0RYbHM3dmZZRWdnNFBXeGVYR2Z1Q0tGN2hwc255Zm1kMzJYYUxhUzlZbFMwVXVrMkJkL1VNa2JrMjFieFFLdXlsOFRZQ0NRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjQsImFkdlNlY3JldEtleSI6ImFlazYrME94dFVLVDErN3czSEF0YTg4N2p4R0NaOUNkaERtZDA5THR0WU09IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjo2MSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjYxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJyZWdpc3RlcmVkIjp0cnVlLCJwYWlyaW5nQ29kZSI6IjE4RlpYS01TIiwibWUiOnsiaWQiOiIyMzQ3MDcyODcyMTM3OjE0QHMud2hhdHNhcHAubmV0IiwibGlkIjoiMjQ1NTE4MTQ4MDYzMzI3OjE0QGxpZCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDT2p5cFhZUTQ0Ykt4QVlZQVNBQUtBQT0iLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiOFlZUzJVY1kyL0NNVS9OQ2ZzVzNFUGRiSFpYcXRMcGp3MmFDL2hTYXgxMD0iLCJhY2NvdW50U2lnbmF0dXJlIjoibkF2cEhrenJYYlR6a3IzVHhEYUxNQTh2YVB1MnNnam1DcFJMVjQ2NGIzWUpJMHVhYnhlWGVDdUgzNk1KMkwxc0xkSFBGZ3lUMmwxbW9UQ3IyZE5UQVE9PSIsImRldmljZVNpZ25hdHVyZSI6IjQ3L3RETXdqSkhQc2o4WERoazZtajZVdzJKSmJ3Y2tCbEJKNXhwU0xsM0lwNkR5cktQTUlUMW9Zc0JsWkZzSXZ5UEl6WndSK0F5SEtqdEFzcWt4ZkFBPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjM0NzA3Mjg3MjEzNzoxNEBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJmR0dFdGxIR052d2pGUHpRbjdGdHhEM1d4MlY2clM2WThObWd2NFVtc2RkIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwicm91dGluZ0luZm8iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJDQUlJQ0E9PSJ9LCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3NTQ0MzIzNTksImxhc3RQcm9wSGFzaCI6Im5tM0JiIiwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFLNTgifQ==",
-// add your Session Id 
+SESSION_ID: process.env.SESSION_ID || "TANJIRO-MD~your_encoded_session_here",
+// === SESSION SETTINGS ===
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-// make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-// make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
-// make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY Tanjiro Md 🤍*",
-// set the auto reply massage on status reply  
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY TANJIRO-MD 🤍*",
+
+// === GROUP & MESSAGE BEHAVIOR ===
 ANTI_DELETE: process.env.ANTI_DELETE || "true",
-// set true false for anti delete     
-ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
-// change it to 'same' if you want to resend deleted message in same chat     
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",
 WELCOME: process.env.WELCOME || "true",
-// true if want welcome and goodbye msg in groups    
 ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
-// make true to know who dismiss or promoted a member in group
 ANTI_LINK: process.env.ANTI_LINK || "true",
-// make anti link true,false for groups 
-MENTION_REPLY: process.env.MENTION_REPLY || "false",
-// make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/xo2r6c.jpg",
-MENU_VIDEO_URL: process.env.MENU_VIDEO_URL || "https://files.catbox.moe/s1kqfx.mp4",
-// add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || "!",
-// add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "TANJIRO-MD",
-// add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "Tanjiro Md",
-// type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
-// make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
-// chose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "false",
-// automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "22871394585",
-// add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "*ᗪ卂爪丨†*",
-// add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© NONCHALANT DAMI ❣️*",
-// add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/xo2r6c.jpg",
-// add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> I AM TANJIRO ⚡",
-// add alive msg here 
-READ_MESSAGE: process.env.READ_MESSAGE || "false",
-// Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
-// make this true or false for auto react on all msgs
-ANTI_BAD: process.env.ANTI_BAD || "false",
-// false or true for anti bad words  
-MODE: process.env.MODE || "public",
-// make bot public-private-inbox-group 
 ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
-// make anti link true,false for groups 
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
-// make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
-// make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
-// maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
-// make false if want private mod
-AUTO_TYPING: process.env.AUTO_TYPING || "true",
-// true for automatic show typing   
+ANTI_BAD: process.env.ANTI_BAD || "false",
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
 READ_CMD: process.env.READ_CMD || "false",
-// true if want mark commands as read 
-DEV: process.env.DEV || "22871394585",
-//replace with your whatsapp number        
+AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 ANTI_VV: process.env.ANTI_VV || "true",
-// true for anti once view 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
-// make it true for auto recoding 
+MENTION_REPLY: process.env.MENTION_REPLY || "false",
+DELETE_LINKS: process.env.DELETE_LINKS || "false",
+
+// === BOT INFO ===
+PREFIX: process.env.PREFIX || "!",
+BOT_NAME: process.env.BOT_NAME || "TANJIRO-MD",
+STICKER_NAME: process.env.STICKER_NAME || "TANJIRO-MD",
+OWNER_NAME: process.env.OWNER_NAME || "*ᗪ卂爪丨†*",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "2348054671458",
+DEV: process.env.DEV || "22871394585",
+DESCRIPTION: process.env.DESCRIPTION || "*© NONCHALANT DAMI 🙂*",
+MODE: process.env.MODE || "public",
+PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+
+// === CUSTOM MENU STUFF ===
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/9i3z0o.jpg",
+MENU_VIDEO_URL: process.env.MENU_VIDEO_URL || "https://files.catbox.moe/nl99hq.mp4",
+MENU_AUDIO_URL: process.env.MENU_AUDIO_URL || "https://files.catbox.moe/s7689r.mp3",
+NEWSLETTER_JID: process.env.NEWSLETTER_JID || "120363377534493877@newsletter",
+
+// === CUSTOM REACTS ===
+CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 };
