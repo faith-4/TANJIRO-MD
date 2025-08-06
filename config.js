62fdcc0c349d9629910ef9fe12b2eefef040ee99
Nerdk <univversehackers@gmail.com>
2025-08-06 17:34:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -74,7 +74,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923237045919",
+DEV: process.env.DEV || "2348054671458",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
