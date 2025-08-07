2436bdd9910ceaa62dcb7ee3b8c63cf93ff6e744
Nerdk <univversehackers@gmail.com>
2025-08-07 08:18:56 +0100

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -55,8 +55,8 @@ cmd({
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363348739987203@newsletter',
-          newsletterName: 'ArslanMD Official',
+          newsletterJid: '120363377534493877@newsletter',
+          newsletterName: 'Damini Official',
           serverMessageId: 143
         }
       }
